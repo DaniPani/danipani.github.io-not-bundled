d95826f9e33aeebe25c9f719609cb3bbfc7bcc9a
Daniel Panero <panero.daniel@gmail.com>
2018-06-30 17:19:39 +0200

    Finished gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -10,9 +10,9 @@ gulp.task('manifest', () => gulp.src('./manifest.json').pipe(gulp.dest('./build/
 gulp.task('serve', () => exec('polymer serve build/default', (err, stdout) => console.log(stdout)))
 gulp.task('build', () => exec('polymer build', (err, stdout) => console.log(stdout)))
 
-gulp.task('clean', () => exec('for %i in (D:\\Projects\\Web\\danipani.github.io\\*) do if not %i == .gitignore del /Q /F %i'))
+gulp.task('clean', () => exec('d: & cd D:\\Projects\\Web\\danipani.github.io\\ &  git rm ./** -f -q --ignore-unmatch'))
 gulp.task('move', () => gulp.src('./build/default/**/*').pipe(gulp.dest('D:/Projects/Web/danipani.github.io/')))
-gulp.task('commit', () => exec(`d: & cd D:\\Projects\\Web\\danipani.github.io\\ & git commit -a -m ${Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 128)} & git push`))
+gulp.task('commit', () => exec(`d: & cd D:\\Projects\\Web\\danipani.github.io\\ & git add -A --no-ignore-removal & git commit -m ${Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 128)} & git push`))
 
 gulp.task('default', gulp.series('build', gulp.parallel('image', 'manifest', 'serve')))
 
