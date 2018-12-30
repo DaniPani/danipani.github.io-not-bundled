189b6728d386ee7faeb2a424a54d4ad6606458b6
Daniel Panero <panero.daniel@gmail.com>
2018-12-30 15:31:45 +0100

    transformed evertything to em units, removed automatic seo stuff

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,72 @@
 const gulp = require('gulp')
 const image = require('gulp-image')
+const gulpSeo = require('gulp-seo')
 
-const {
-  exec
-} = require('child_process');
+const { exec } = require('child_process')
 
-gulp.task('image', () => gulp.src('./build/default/src/image/*').pipe(image()).pipe(gulp.dest('./build/default/src/image/')))
+gulp.task('image', () =>
+  gulp
+    .src('./build/default/src/image/*')
+    .pipe(image())
+    .pipe(gulp.dest('./build/default/src/image/'))
+)
 
-gulp.task('serve', () => exec('polymer serve build/default', (err, stdout) => console.log(err, stdout)))
-gulp.task('build', () => exec('polymer build', (err, stdout) => console.log(stdout)))
+gulp.task('serve', () =>
+  exec('polymer serve build/default', (err, stdout) => console.log(err, stdout))
+)
+gulp.task('build', () =>
+  exec('polymer build', (err, stdout) => {
+    if (err) {
+      console.log(stdout)
+    }
+  })
+)
 
-gulp.task('clean', () => exec('d: & cd D:\\Projects\\Web\\danipani.github.io\\ &  git rm ./** -f -q --ignore-unmatch'))
-gulp.task('move', () => gulp.src('./build/default/**/*').pipe(gulp.dest('D:/Projects/Web/danipani.github.io/')))
-gulp.task('commit', () => exec(`d: & cd D:\\Projects\\Web\\danipani.github.io\\ & git add -A --no-ignore-removal & git commit -m ${Math.random().toString(36).replace(/[^a-z0-9]+/g, '').substr(0, 128)} & git push`))
+gulp.task('seo', () =>
+  gulp
+    .src('build/default/index.html')
+    .pipe(
+      gulpSeo({
+        list: ['og', 'se', 'schema', 'twitter'],
+        meta: {
+          title: "A DaniPani's (Daniel Panero) Portfolio",
+          description:
+            "Hi, I'm Daniel, a junior Developer and welcome to my Portfolio, which is completely written in ES6 Features with LitElement and Polymer.",
+          author: 'DaniPani Portfolio',
+          image:
+            'https://danipani.github.io/src/image/abstract-wallpaper-47342-48869-hd-wallpapers.jpg',
+          site_name: 'DaniPani Portfolio',
+          type: 'website',
+          concact: 'panero.daniel@gmail.com',
+          url: 'https://danipani.github.io'
+        }
+      })
+    )
+    .pipe(gulp.dest('build/default/'))
+)
 
-gulp.task('default', gulp.series('build', gulp.parallel('image', 'serve')))
+gulp.task('clean', () =>
+  exec(
+    'd: & cd D:\\Projects\\Web\\danipani.github.io\\ &  git rm ./** -f -q --ignore-unmatch'
+  )
+)
+gulp.task('move', () =>
+  gulp
+    .src('./build/default/**/*')
+    .pipe(gulp.dest('D:/Projects/Web/danipani.github.io/'))
+)
+gulp.task('commit', () =>
+  exec(
+    `d: & cd D:\\Projects\\Web\\danipani.github.io\\ & git add -A --no-ignore-removal & git commit -m ${Math.random()
+      .toString(36)
+      .replace(/[^a-z0-9]+/g, '')
+      .substr(0, 128)} & git push`
+  )
+)
 
-gulp.task('deploy', gulp.series('build', gulp.parallel('image', 'clean'), 'move', 'commit'))
+gulp.task('default', gulp.series('build', 'seo', gulp.parallel('image', 'serve')))
+
+gulp.task(
+  'deploy',
+  gulp.series('build', gulp.parallel('image', 'clean', 'seo'), 'move', 'commit')
+)
